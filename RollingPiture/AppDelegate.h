495e6e995d18e29339f37f6c7e4df28491186092
yandeguang <754199572@qq.com>
2016-03-21 17:56:39 +0800

    Creat Project

---
+++ RollingPiture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RollingPiture
+//
+//  Created by FSLB on 16/3/21.
+//  Copyright © 2016年 FSLB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RollingPiture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RollingPiture
+//
+//  Created by FSLB on 16/3/21.
+//  Copyright © 2016年 FSLB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
