bc2eec2e25ade5975f4605f78b238b12956d1d23
yandeguang <754199572@qq.com>
2016-03-29 15:10:09 +0800

    增加pod spec文件

--- RollingPiture/ViewController.m
+++ RollingPiture/ViewController.m
@@ -24,57 +24,23 @@
     _rollingView.dataSource = self;
     [self.view addSubview:_rollingView];
     
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = CGRectMake(100, 300, 90, 90);
-    btn.backgroundColor = [UIColor cyanColor];
-    [self.view addSubview:btn];
     
-    [btn setImage:[UIImage imageNamed:@"21"] forState:UIControlStateNormal];
-     btn.imageEdgeInsets = UIEdgeInsetsMake(0,24,50,24);
-    
-    [btn setTitle:@"首页" forState:UIControlStateNormal];
-    btn.titleLabel.frame = CGRectMake(0, 0, btn.bounds.size.width, 16);
-    btn.titleLabel.font = [UIFont systemFontOfSize:16];
-    btn.titleLabel.textAlignment = NSTextAlignmentCenter;
-    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [btn setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
-     btn.titleEdgeInsets = UIEdgeInsetsMake(40, 0, 0, 0);
-    
-    btn.imageView.backgroundColor = [UIColor redColor];
-    btn.titleLabel.backgroundColor = [UIColor blackColor];
-    
-    btn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-    btn.contentVerticalAlignment = UIControlContentVerticalAlignmentTop;
-    NSLog(@"%f",btn.imageView.bounds.origin.x);
-    
-    [btn addTarget:self action:@selector(test:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (NSMutableArray *)dataArray{
     if(!_dataArray){
-        _dataArray = [[NSMutableArray alloc] init];
-//        _dataArray = [[NSMutableArray alloc] initWithObjects:
-//                      @"http://203.130.45.116:8001/imgUpload/pics20160522060507152.png",
-//                      @"http://203.130.45.116:8001/imgUpload/pics20161919031954975.jpg",
-//                      @"http://203.130.45.116:8001/imgUpload/pics2016040602042036.png",
-//                      @"http://203.130.45.116:8001/imgUpload/pics20162405042453466.jpg",
-//                      @"http://203.130.45.116:8001/imgUpload/pics20160606020645104.jpg", nil];
+        _dataArray = [[NSMutableArray alloc] initWithObjects:
+                      @"http://203.130.45.116:8001/imgUpload/pics20160522060507152.png",
+                      @"http://203.130.45.116:8001/imgUpload/pics20161919031954975.jpg",
+                      @"http://203.130.45.116:8001/imgUpload/pics2016040602042036.png",
+                      @"http://203.130.45.116:8001/imgUpload/pics20162405042453466.jpg",
+                      @"http://203.130.45.116:8001/imgUpload/pics20160606020645104.jpg", nil];
 
     }
     return _dataArray;
 }
 
-- (void)test:(id)sender {
-    _dataArray = [[NSMutableArray alloc] initWithObjects:
-                                        @"http://203.130.45.116:8001/imgUpload/pics20160522060507152.png",
-                                        @"http://203.130.45.116:8001/imgUpload/pics20161919031954975.jpg",
-                                        @"http://203.130.45.116:8001/imgUpload/pics2016040602042036.png",
-                                        @"http://203.130.45.116:8001/imgUpload/pics20162405042453466.jpg",
-                                        @"http://203.130.45.116:8001/imgUpload/pics20160606020645104.jpg", nil];
-    [_rollingView reloadDataWithCompleteBlock:^{
-        NSLog(@"2");
-    }];
-}
+
 
 - (UICollectionViewScrollDirection) DGRollingViewDirection{
     return UICollectionViewScrollDirectionHorizontal;
